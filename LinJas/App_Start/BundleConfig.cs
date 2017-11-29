b4589992e2bc4dda34210119e97f1c17afb3b6f9
anhtc <phonghanhchinh21@gmail.com>
2017-11-29 09:42:16 +0700

    mới nhất

--- LinJas/App_Start/BundleConfig.cs
+++ LinJas/App_Start/BundleConfig.cs
@@ -8,24 +8,51 @@ namespace LinJas
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            #region Desktop
+            //common
+            bundles.Add(new ScriptBundle("~/bundles/js/jquery.common.js").Include(
+                    "~/js/jquery-1.10.2.min.js"));
+            //Home
+            bundles.Add(new ScriptBundle("~/bundles/js/home.index.desktop.js").Include(
+                     "~/js/bootstrap.min.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new StyleBundle("~/css/home.index.desktop.css").Include(
+                     "~/css/bootstrap.min.css",
+                     "~/css/linja.common.desktop.css"));
+            //Contact
+            bundles.Add(new ScriptBundle("~/bundles/js/contact.desktop.js").Include(
+                    "~/js/bootstrap.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new StyleBundle("~/css/contact.desktop.css").Include(
+                     "~/css/bootstrap.min.css",
+                     "~/css/linja.common.desktop.css",
+                     "~/css/linja.contact.desktop.css"));
+            //About
+            bundles.Add(new ScriptBundle("~/bundles/js/about.desktop.js").Include(
+                    "~/js/bootstrap.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/css/about.desktop.css").Include(
+                     "~/css/bootstrap.min.css",
+                     "~/css/linja.common.desktop.css",
+                     "~/css/linja.about.desktop.css"));
+            //Search 
+            bundles.Add(new ScriptBundle("~/bundles/js/search.desktop.js").Include(
+                    "~/js/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/css/search.desktop.css").Include(
+                     "~/css/bootstrap.min.css",
+                     "~/css/linja.common.desktop.css",
+                     "~/css/linja.search.desktop.css"));
+            #endregion
+
+            #region Mobile
+
+            #endregion
+
+
+
+
         }
     }
 }
