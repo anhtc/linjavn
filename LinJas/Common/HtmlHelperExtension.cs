f3fe9c9bb3360528838b76b49478cdbdbd3bd330
anhtc <phonghanhchinh21@gmail.com>
2017-11-29 09:45:48 +0700

    update

---
+++ LinJas/Common/HtmlHelperExtension.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using log4net;
+using System.Globalization;
+using System.IO;
+using System.Web.Optimization;
+
+namespace LinJas.Common
+{
+    public static class HtmlHelperExtension
+    {
+        public static string CategoryItemActive(this HtmlHelper html, string actions = "", string controllers = "",
+            string categorys = "", string cssClass = "active")
+        {
+            try
+            {
+                var num = 0;
+                ViewContext viewContext = html.ViewContext;
+                bool isChildAction = viewContext.Controller.ControllerContext.IsChildAction;
+
+                if (isChildAction)
+                    viewContext = html.ViewContext.ParentActionViewContext;
+
+
+                RouteValueDictionary routeValues = viewContext.RouteData.Values;
+                string currentAction = routeValues["action"].ToString();
+                string currentController = routeValues["controller"].ToString();
+
+                string currentCategory = "";
+                if (routeValues.ContainsKey("category"))
+                {
+                    currentCategory = routeValues["category"].ToString();
+                }
+                else if (int.TryParse(categorys, out num) && routeValues.ContainsKey("parentId"))
+                {
+                    currentCategory = routeValues["parentId"].ToString();
+                }
+                else if (routeValues.ContainsKey("tenLoaiBlog"))
+                {
+                    currentCategory = routeValues["tenLoaiBlog"].ToString();
+                }
+                else if (int.TryParse(categorys, out num) && routeValues.ContainsKey("loaiTinTucId"))
+                {
+                    currentCategory = routeValues["loaiTinTucId"].ToString();
+                }
+                else if (int.TryParse(categorys, out num) && routeValues.ContainsKey("tagId"))
+                {
+                    currentCategory = routeValues["tagId"].ToString();
+                }
+                else if (int.TryParse(categorys, out num) && routeValues.ContainsKey("nhomBSTId"))
+                {
+                    currentCategory = routeValues["nhomBSTId"].ToString();
+                }
+                if (String.IsNullOrEmpty(actions))
+                    actions = currentAction;
+
+                if (String.IsNullOrEmpty(controllers))
+                    controllers = currentController;
+
+                if (String.IsNullOrEmpty(categorys))
+                    categorys = currentCategory;
+
+                string[] acceptedActions = actions.Trim().Split(',').Distinct().ToArray();
+                string[] acceptedControllers = controllers.Trim().Split(',').Distinct().ToArray();
+                string[] acceptedCategorys = categorys.Trim().Split(',').Distinct().ToArray();
+
+                string str = acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController) &&
+                             acceptedCategorys.Contains(currentCategory)
+                    ? cssClass
+                    : String.Empty;
+
+                return acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController) &&
+                       acceptedCategorys.Contains(currentCategory)
+                    ? cssClass
+                    : String.Empty;
+            }
+            catch (Exception ex)
+            {                
+                throw ex;
+            }
+        }
+
+
+        public static string ReturnUrl(this HttpRequestBase request)
+        {
+            var returnUrl = request.RawUrl;
+            if (!string.IsNullOrEmpty(request.QueryString["returnUrl"]))
+            {
+                returnUrl = request.QueryString["returnUrl"];
+            }
+            return returnUrl;
+        }
+      
+        private static string LoadBundleContent(HttpContextBase httpContext, string bundleVirtualPath)
+        {
+            var bundleContext = new BundleContext(httpContext, BundleTable.Bundles, bundleVirtualPath);
+            var bundle = BundleTable.Bundles.Single(b => b.Path == bundleVirtualPath);
+            var bundleResponse = bundle.GenerateBundleResponse(bundleContext);
+
+            return bundleResponse.Content;
+        }
+
+        private static IHtmlString InlineBundle(this HtmlHelper htmlHelper, string bundleVirtualPath, string htmlTagName)
+        {
+            string bundleContent = LoadBundleContent(htmlHelper.ViewContext.HttpContext, bundleVirtualPath);
+            string htmlTag = string.Format("<{0}>{1}</{0}>", htmlTagName, bundleContent);
+
+            return new HtmlString(htmlTag);
+        }
+
+        public static IHtmlString InlineScripts(this HtmlHelper htmlHelper, string bundleVirtualPath)
+        {
+            return htmlHelper.InlineBundle(bundleVirtualPath, "script");
+        }
+
+        public static IHtmlString InlineStyles(this HtmlHelper htmlHelper, string bundleVirtualPath)
+        {
+            return htmlHelper.InlineBundle(bundleVirtualPath, "style");
+        }      
+        /* Bread crum */
+        public static string Titleize(this string text)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text).ToSentenceCase();
+        }
+        public static string ToSentenceCase(this string str)
+        {
+            return Regex.Replace(str, "[a-z][A-Z]", m => m.Value[0] + " " + char.ToLower(m.Value[1]));
+        }
+    }
+}
---
+++ LinJas/Common/Utilities.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Globalization;
+using System.Linq;
+using System.Web.Configuration;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace LinJas.Common
+{
+    public static class Utilities
+    {
+        public static string GetDay(int i)
+        {
+            int day = (int)DateTime.Now.Date.AddDays(i).DayOfWeek;
+            string value = "";
+            switch (day)
+            {
+                case 1:
+                    value = "Thứ 2";
+                    break;
+                case 2:
+                    value = "Thứ 3";
+                    break;
+                case 3:
+                    value = "Thứ 4";
+                    break;
+                case 4:
+                    value = "Thứ 5";
+                    break;
+                case 5:
+                    value = "Thứ 6";
+                    break;
+                case 6:
+                    value = "Thứ 7";
+                    break;
+                default:
+                    value = "Chủ nhật";
+                    break;
+            }
+            return value;
+        }
+        public static string GetValueByKey(string key)
+        {
+            try
+            {
+                var value = WebConfigurationManager.AppSettings[key];
+                return value;
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+
+        private static readonly MD5 Md5Hash = MD5.Create();
+
+        /// <summary>
+        /// Determines if a nullable Guid (Guid?) is null or Guid.Empty
+        /// </summary>
+        public static bool IsNullOrEmpty(this Guid? guid)
+        {
+            return (!guid.HasValue || guid.Value == Guid.Empty);
+        }
+        public static string GetMd5Hash(string input)
+        {
+            byte[] data = Md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                builder.Append(data[i].ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+        public static string GetClientIP()
+        {
+            var ipAddress = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                var addresses = ipAddress.Split(',');
+                if (addresses.Length != 0)
+                {
+                    return addresses[0];
+                }
+            }
+
+            return HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+        }
+
+        public static string RemoveStyle(string html) {
+            try
+            {
+                string cleaned;
+                cleaned = new Regex("style=\"[^\"]*\"").Replace(html, "");
+                cleaned = new Regex("(?<=class=\")([^\"]*)\\babc\\w*\\b([^\"]*)(?=\")").Replace(cleaned, "$1$2");
+                return cleaned;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        //loại bỏ  các thẻ html,style tự sinh ra khi nhập liệu
+        public static string ReplaceHtml(string Txt)
+        {
+            Txt = Regex.Replace(Txt, "<(.|\\n)*?>", string.Empty);
+            return Txt;
+        }
+    }
+}
