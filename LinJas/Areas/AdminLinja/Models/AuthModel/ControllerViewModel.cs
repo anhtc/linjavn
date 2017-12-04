eefd916f3b6dd51f1895ebb5f9b0c885ad392d78
anhtc <phonghanhchinh21@gmail.com>
2017-12-04 10:15:24 +0700

    update  menu

---
+++ LinJas/Areas/AdminLinja/Models/AuthModel/ControllerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LinJas.Areas.AdminLinja.Models.AuthModel
+{
+    public class ControllerViewModel
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Attributes { get; set; }
+    }    
+    public class ControllerRoleViewModel
+    {
+        public List<AspRoleController> ControllerSelecteds { get; set; }
+        public List<AspController> Controllers { get; set; }
+        public AspNetRole Roles { get; set; }
+    }
+}
